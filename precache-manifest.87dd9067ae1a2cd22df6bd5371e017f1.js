7f477d83d2e9b432e1e5041d3c03191d54d7de02
Lulin-S <70949875+Lulin-S@users.noreply.github.com>
2021-03-19 00:02:20 +0100

    Production pages for Tickethub project

---
+++ precache-manifest.87dd9067ae1a2cd22df6bd5371e017f1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586ff9a1ff1e4ae1a37e",
+    "url": "css/app.7a2e2c53.css"
+  },
+  {
+    "revision": "77e260cb74c1c7407175",
+    "url": "css/chunk-0bbd4eb4.9c24fb72.css"
+  },
+  {
+    "revision": "747e35c036d1755c5a4a",
+    "url": "css/chunk-0c7d1c04.bc607aae.css"
+  },
+  {
+    "revision": "47724d34274a5633c91d",
+    "url": "css/chunk-65ae4639.fe74a2ca.css"
+  },
+  {
+    "revision": "c8e6b38efe25561dd05efcfff9325f13",
+    "url": "img/concertsbanner1.c8e6b38e.jpg"
+  },
+  {
+    "revision": "4970eddc90aba76d2ccc475b709978eb",
+    "url": "index.html"
+  },
+  {
+    "revision": "586ff9a1ff1e4ae1a37e",
+    "url": "js/app.33621365.js"
+  },
+  {
+    "revision": "77e260cb74c1c7407175",
+    "url": "js/chunk-0bbd4eb4.4fb5830b.js"
+  },
+  {
+    "revision": "747e35c036d1755c5a4a",
+    "url": "js/chunk-0c7d1c04.91c668cf.js"
+  },
+  {
+    "revision": "47724d34274a5633c91d",
+    "url": "js/chunk-65ae4639.def55427.js"
+  },
+  {
+    "revision": "b0be8d475bd3c1a672cb",
+    "url": "js/chunk-vendors.3e6faff4.js"
+  },
+  {
+    "revision": "3c424340bd6d5c7e7a7b9b83fdd25be1",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
